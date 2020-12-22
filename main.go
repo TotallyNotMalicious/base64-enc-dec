2dcf28ef52372f948bff06371c2a50a81a6e3962
Malicious <61036834+TotallyNotMalicious@users.noreply.github.com>
2020-12-22 11:52:56 -0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	b64 "encoding/base64"
+	"fmt"
+)
+
+var input string
+
+func encrypt(input string) {
+	encoded := b64.StdEncoding.EncodeToString([]byte(input))
+	fmt.Println()
+	fmt.Println(encoded)
+}
+
+func decrypt(input string) {
+	decoded, _ := b64.StdEncoding.DecodeString(input)
+	fmt.Println()
+	fmt.Println(string(decoded))
+}
+
+func main() {
+
+	fmt.Printf("\nDo You Want To Encrypt A String Or Decrypt A String?: ")
+	fmt.Scanln(&input)
+
+	if input == "encrypt" || input == "ENCRYPT" {
+
+		fmt.Printf("\nWhat Is The String You Would Like To Encrypt?: ")
+		fmt.Scanln(&input)
+		encrypt(input)
+
+	} else if input == "decrypt" || input == "DECRYPT" {
+
+		fmt.Printf("\nWhat Is The String You Would Like To Decrypt?: ")
+		fmt.Scanln(&input)
+		decrypt(input)
+	}
+}

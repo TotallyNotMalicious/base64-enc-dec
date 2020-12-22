de1609c24721191d60f0cec422e70d72e62faa11
Malicious <61036834+TotallyNotMalicious@users.noreply.github.com>
2020-12-22 14:00:58 -0800

    Update main.go

--- main.go
+++ main.go
@@ -21,18 +21,18 @@ func decrypt(input string) {
 
 func main() {
 
-	fmt.Printf("\nDo You Want To Encrypt A String Or Decrypt A String?: ")
+	fmt.Print("\nDo You Want To Encrypt A String Or Decrypt A String?: ")
 	fmt.Scanln(&input)
 
 	if input == "encrypt" || input == "ENCRYPT" {
 
-		fmt.Printf("\nWhat Is The String You Would Like To Encrypt?: ")
+		fmt.Print("\nWhat Is The String You Would Like To Encrypt?: ")
 		fmt.Scanln(&input)
 		encrypt(input)
 
 	} else if input == "decrypt" || input == "DECRYPT" {
 
-		fmt.Printf("\nWhat Is The String You Would Like To Decrypt?: ")
+		fmt.Print("\nWhat Is The String You Would Like To Decrypt?: ")
 		fmt.Scanln(&input)
 		decrypt(input)
 	}
